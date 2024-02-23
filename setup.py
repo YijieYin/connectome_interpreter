d0238585ca4af02a0969e1ed7c4c4cea604de2ea
YijieYin <yy432@cam.ac.uk>
2024-02-23 16:00:11 +0000

    change homepage link

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='connectome_interpreter',
-    version='0.1',
+    # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
+    # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
+    # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
@@ -11,6 +14,7 @@ setup(
         'scipy',
         'torch',
         'tqdm',
+        'plotly',
     ],
     # Optional metadata
     author='Yijie Yin',
@@ -18,5 +22,5 @@ setup(
     description='A tool for connectomics data interpretation',
     keywords=['connectomics', 'neural network',
               'mechanistic interpretability'],
-    url='http://example.com/MyPackage',  # Project home page
+    url='https://connectome-interpreter.readthedocs.io/en/latest/',  # Project home page
 )
