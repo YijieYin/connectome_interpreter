7bee0b018bd0f1ec869c51430de39546741ac057
YijieYin <yy432@cam.ac.uk>
2024-03-02 15:20:50 +0000

    change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
     # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
     # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
@@ -22,5 +22,8 @@ setup(
     description='A tool for connectomics data interpretation',
     keywords=['connectomics', 'neural network',
               'mechanistic interpretability'],
-    url='https://connectome-interpreter.readthedocs.io/en/latest/',  # Project home page
+    project_urls={
+        'Documentation': 'https://connectome-interpreter.readthedocs.io/en/latest/',
+    }
+    # url='https://connectome-interpreter.readthedocs.io/en/latest/',  # Project home page
 )
