df89c200f5482403b7a5fd9f494e9e25d8a1039c
YijieYin <yy432@cam.ac.uk>
2024-03-07 09:33:32 +0000

    change version number and add example links

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
     # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
     # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
-    version='0.4.0',
+    version='0.4.2',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
@@ -28,6 +28,8 @@ setup(
               'mechanistic interpretability'],
     project_urls={
         'Documentation': 'https://connectome-interpreter.readthedocs.io/en/latest/',
+        'Larva connectome example': 'https://colab.research.google.com/drive/1VIMNFBp7dCgN5XOQ9vvzPaqb80BGPZx4?usp=sharing',
+        'Adult connectome example (FAFB)': 'https://colab.research.google.com/drive/1ECUagwN-r2rnKyfcYgtR1oG8Lox8m8BW?usp=sharing',
     }
     # url='https://connectome-interpreter.readthedocs.io/en/latest/',  # Project home page
 )
