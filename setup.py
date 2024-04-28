74f0da477a32f8d7818485244c66d80d7576755a
YijieYin <yy432@cam.ac.uk>
2024-04-28 19:01:53 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
     # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
     # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
-    version='1.4.1',
+    version='1.6.0',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
@@ -17,6 +17,8 @@ setup(
         'plotly',
         'matplotlib',
         'networkx',
+        'seaborn',
+        'ipywidgets',
     ],
     extras_require={
         'get_ngl_link': ['nglscenes'],
@@ -29,6 +31,7 @@ setup(
     keywords=['connectomics', 'neural network',
               'mechanistic interpretability'],
     project_urls={
+        'Source': 'https: // github.com/YijieYin/connectome_interpreter',
         'Documentation': 'https://connectome-interpreter.readthedocs.io/en/latest/',
         'Larva connectome example': 'https://colab.research.google.com/drive/1VIMNFBp7dCgN5XOQ9vvzPaqb80BGPZx4?usp=sharing',
         'Adult connectome example (FAFB)': 'https://colab.research.google.com/drive/1ECUagwN-r2rnKyfcYgtR1oG8Lox8m8BW?usp=sharing',
