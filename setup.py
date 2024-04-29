8c5f837174e9948c90c726d77f87e6af4a9de0c6
YijieYin <yy432@cam.ac.uk>
2024-04-29 18:47:18 +0100

    another attempt to not install torch again

--- setup.py
+++ setup.py
@@ -4,19 +4,30 @@ import pkg_resources
 # Function to check if PyTorch is already installed
 
 
-def get_torch_version():
+def is_torch_installed():
     try:
-        return pkg_resources.get_distribution('torch').version
+        pkg_resources.get_distribution('torch')
+        return True
     except pkg_resources.DistributionNotFound:
-        return None
+        return False
 
 
-# Specify a PyTorch version requirement only if it's not installed
-torch_version = get_torch_version()
-if torch_version is None:
-    torch_requirement = 'torch>=1.7.1'
-else:
-    torch_requirement = f'torch=={torch_version}'
+install_requires_list = [
+    # List your package dependencies here
+    'numpy',
+    'pandas',
+    'scipy',
+    'tqdm',
+    'plotly',
+    'matplotlib',
+    'networkx',
+    'seaborn',
+    'ipywidgets',
+    'IPython',
+]
+# Add torch only if it's not already installed
+if not is_torch_installed():
+    install_requires_list.append('torch>=1.7.1')
 
 
 setup(
@@ -24,23 +35,9 @@ setup(
     # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
     # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
     # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
-    version='1.6.3',
+    version='1.6.4',
     packages=find_packages(),
-    install_requires=[
-        # List your package dependencies here
-        'numpy',
-        'pandas',
-        'scipy',
-        # 'torch>=1.7.1',
-        torch_requirement,
-        'tqdm',
-        'plotly',
-        'matplotlib',
-        'networkx',
-        'seaborn',
-        'ipywidgets',
-        'IPython',
-    ],
+    install_requires=install_requires_list,
     extras_require={
         'get_ngl_link': ['nglscenes'],
         'wandb': ['wandb'],
