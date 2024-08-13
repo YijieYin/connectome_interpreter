72fff231126a1bfe91f8966f0b8f5885a0b2de78
YijieYin <yy432@cam.ac.uk>
2024-08-13 23:50:50 +0100

    update version number

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
     # If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
     # If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
-    version='1.11.0',
+    version='1.12.0',
     packages=find_packages(),
     install_requires=install_requires_list,
     extras_require={
@@ -51,13 +51,10 @@ setup(
     author='Yijie Yin',
     author_email='yy432@cam.ac.uk',
     description='A tool for connectomics data interpretation',
-    keywords=['connectomics', 'neural network',
-              'mechanistic interpretability'],
+    keywords=['connectomics', 'neural network'],
     project_urls={
-        'Source': 'https: // github.com/YijieYin/connectome_interpreter',
+        'Source & example notebooks': 'https://github.com/YijieYin/connectome_interpreter',
         'Documentation': 'https://connectome-interpreter.readthedocs.io/en/latest/',
-        'Adult connectome example (FAFB)': 'https://colab.research.google.com/drive/1ECUagwN-r2rnKyfcYgtR1oG8Lox8m8BW?usp=sharing',
-        'Larva connectome example': 'https://colab.research.google.com/drive/1VIMNFBp7dCgN5XOQ9vvzPaqb80BGPZx4?usp=sharing',
     },
     # url='https://connectome-interpreter.readthedocs.io/en/latest/',
     test_suite='tests'  # Project home page
