465dcff4ca3d21f37e295ce23511e8db040ebfd4
YijieYin <yy432@cam.ac.uk>
2024-11-07 23:59:17 +0000

    allow query version

--- connectome_interpreter/__init__.py
+++ connectome_interpreter/__init__.py
@@ -1,2 +1,3 @@
 from . import compress_paths, path_finding, utils, activation_maximisation, external_map
 # from .activation_maximisation import MultilayeredNetwork
+from ._version import __version__
---
+++ connectome_interpreter/_version.py
@@ -0,0 +1,4 @@
+__version__ = '1.13.0'
+# If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
+# If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
+# If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
