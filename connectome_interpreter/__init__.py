92b17e2de0b8e74be4721de8f47c40907fcc6cde
YijieYin <yy432@cam.ac.uk>
2025-01-03 15:39:48 +0000

    import all functions upon import

--- connectome_interpreter/__init__.py
+++ connectome_interpreter/__init__.py
@@ -1,3 +1,7 @@
-from . import compress_paths, path_finding, utils, activation_maximisation, external_map
-# from .activation_maximisation import MultilayeredNetwork
+from .compress_paths import *
+from .path_finding import *
+from .utils import *
+from .activation_maximisation import *
+from .external_map import *
 from ._version import __version__
+# from .activation_maximisation import MultilayeredNetwork
