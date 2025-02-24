e27a77e75b0d1f6cc1d94d0dcb899497b338a903
YijieYin <yy432@cam.ac.uk>
2025-02-24 01:14:09 +0000

    bump version

--- connectome_interpreter/_version.py
+++ connectome_interpreter/_version.py
@@ -1,4 +1,7 @@
-__version__ = '2.1.2'
-# If you're making a patch or a minor bug fix, increment the patch version, e.g., from 0.1.0 to 0.1.1.
-# If you're adding functionality in a backwards-compatible manner, increment the minor version, e.g., from 0.1.0 to 0.2.0.
-# If you're making incompatible API changes, increment the major version, e.g., from 0.1.0 to 1.0.0.
+__version__ = "2.2.0"
+# If you're making a patch or a minor bug fix, increment the patch version,
+# e.g., from 0.1.0 to 0.1.1.
+# If you're adding functionality in a backwards-compatible manner, increment
+# the minor version, e.g., from 0.1.0 to 0.2.0.
+# If you're making incompatible API changes, increment the major version, e.g.,
+# from 0.1.0 to 1.0.0.
