ae1c47a851519619372fecf2624bb4825189216e
hoellerj <96149668+hoellerj@users.noreply.github.com>
2025-05-20 13:02:00 -0400

    add info flow script (#18)
    * add info flow script
    
    * include external_paths in init and docs
    
    ---------
    
    Co-authored-by: YijieYin <yy432@cam.ac.uk>

--- connectome_interpreter/__init__.py
+++ connectome_interpreter/__init__.py
@@ -4,5 +4,6 @@ from .compress_paths import *
 from .external_map import *
 from .path_finding import *
 from .utils import *
+from .external_paths import *
 
 # from .activation_maximisation import MultilayeredNetwork
